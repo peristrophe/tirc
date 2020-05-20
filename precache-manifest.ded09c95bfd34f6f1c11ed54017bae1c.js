f0718ba4f9eaf864e9f9fb3f4ec433f3f9f14278
peristrophe <peristrophe@users.noreply.github.com>
2020-05-20 12:10:00 +0000

    deploy: peristrophe/twitch-irc-client-pwa@b3322189898bc5104363c2b6a60b082ffd6c433c

---
+++ precache-manifest.ded09c95bfd34f6f1c11ed54017bae1c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0761164062b7e1194604",
+    "url": "/twitch-irc-client-pwa/css/app.css"
+  },
+  {
+    "revision": "4a01352abfde13244c5f",
+    "url": "/twitch-irc-client-pwa/css/chunk-vendors.css"
+  },
+  {
+    "revision": "d182d16fd7850b63d2abfc57233a7554",
+    "url": "/twitch-irc-client-pwa/img/ThumbAuthPW.png"
+  },
+  {
+    "revision": "836e7a8731afaf941ec5093dc41225ed",
+    "url": "/twitch-irc-client-pwa/index.html"
+  },
+  {
+    "revision": "0761164062b7e1194604",
+    "url": "/twitch-irc-client-pwa/js/app.js"
+  },
+  {
+    "revision": "4a01352abfde13244c5f",
+    "url": "/twitch-irc-client-pwa/js/chunk-vendors.js"
+  },
+  {
+    "revision": "496c1ceec1173bbb1b06081c2d6edc50",
+    "url": "/twitch-irc-client-pwa/manifest.json"
+  },
+  {
+    "revision": "f87a22f64b1a2bbbe1cb1e51fd493464",
+    "url": "/twitch-irc-client-pwa/media/SoundEffect001.mp3"
+  },
+  {
+    "revision": "bcceea259d8368b3f2443157a128843a",
+    "url": "/twitch-irc-client-pwa/media/SoundEffect002.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/twitch-irc-client-pwa/robots.txt"
+  }
+]);
